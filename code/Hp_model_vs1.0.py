5fee748be9755d7a5f7e6437aad88338c7d83fdb
Francesco Bena <francesco.bena@studenti.unipd.it>
2024-11-05 15:42:16 +0100

    nuovo commento

--- code/Hp_model_vs1.0.py
+++ code/Hp_model_vs1.0.py
@@ -2,11 +2,13 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 
-from code.models import model_h01d01, model_h01d02
+from models import model_h01d01, model_h01d02
 
-df=pd.read_excel(os.path.join('1--Data','Galletti MLI 18 kW.xlsx'), sheet_name="SetData")
+df=pd.read_excel(os.path.join('Data','Galletti MLI 18 kW.xlsx'), sheet_name="SetData")
 
 Y_predict, Y, MSE, RMSE = model_h01d01(df)
+
+# commento
 
 "Plot"
 fig,[ax1,ax2]=plt.subplots(nrows=1,ncols=2)
